9192b72e66e7dbea3fc93756b820a8a75e2b9f56
KimKonG <tigerkimkg@mail.ru>
2025-12-17 18:59:57 +0300

    HW (lection 3) is done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
 function compareArrays(arr1, arr2) {
-  
+  const arrOfArrs = [];
+  arrOfArrs.push(arr1, arr2);
+  if (arrOfArrs.every(item => JSON.stringify(item) === JSON.stringify(arr1))) {
+    return true;
+  } else {
+      return false;  
+  }
 }
+ 
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = users.filter(person => person.gender === gender)
+    .map(person => person.age)
+    .reduce((acc, item, index, arr) => {
+        acc += item;
+        if (index === arr.length - 1) {
+            return acc / arr.length;
+        }
+        return acc;
+    }, 0)
+  return result;  
 }

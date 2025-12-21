dcfce2f6ce9f3bbed15d9fb3acdb9a825847ceb3
KimKonG <tigerkimkg@mail.ru>
2025-12-21 18:11:07 +0300

    HW (lection 4) is done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (this.marks != undefined) {
+		this.marks = [...this.marks, ...marks];
+	}
+
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (this.marks === undefined || this.marks.length === 0) {
+		return 0;
+	} else {
+		return this.marks.reduce((acc, mark, index, arr) => {
+			acc += mark;
+			if (index === arr.length - 1) {
+				return acc / arr.length;
+			}
+			return acc;
+		}, 0)
+	}
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }

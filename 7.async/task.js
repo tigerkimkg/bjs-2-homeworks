029c0b0bc5659e6775da7ae7eed7fe2895489698
KimKonG <tigerkimkg@mail.ru>
2026-01-06 13:41:26 +0300

    HW (lection 7) is done

---
+++ 7.async/task.js
@@ -0,0 +1,68 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback) {
+        if (!time || !callback) {
+            throw new Error("Отсутствуют обязательные аргументы");
+        }
+
+        if (this.alarmCollection.some(alarm => alarm.time === time)) {
+            console.warn("Уже присутствует звонок на это время");
+        }
+
+        const newAlarm = {
+            time,
+            callback,
+            canCall: true
+        };
+        this.alarmCollection.push(newAlarm);
+    }
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+    }
+
+    getCurrentFormattedTime() {
+        const now = new Date();
+        let hours = now.getHours();
+        let minutes = now.getMinutes();
+
+        return `${hours}:${minutes}`;
+    }
+
+    start() {
+
+        if (!!this.intervalId) {
+            return;
+        }
+
+        this.intervalId = setInterval(() => {
+            const currentTime = this.getCurrentFormattedTime();
+            this.alarmCollection.forEach(alarm => {
+                if (alarm.time === currentTime && alarm.canCall) {
+                    alarm.canCall = false;
+                    alarm.callback();
+                }
+            });
+        }, 1000);
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(alarm => {
+            alarm.canCall = true;
+        });
+    }
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+}

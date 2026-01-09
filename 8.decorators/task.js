dd5786ad44248e0516f55d1d9f27bc7ad550d6f4
KimKonG <tigerkimkg@mail.ru>
2026-01-09 17:27:38 +0300

    HW (lection 8) is done

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,52 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = md5(JSON.stringify(args)); // получаем правильный хеш c помощью функции md5
+        let objectInCache = cache.find((item) => item.hash === hash); // ищем элемент, хеш которого равен нашему хешу
+        if (objectInCache) { // если элемент найден
+            console.log("Из кеша: " + objectInCache.value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+            return "Из кеша: " + objectInCache.value;
+        }
+
+        let result = func(...args); // в кеше результата нет — придётся считать
+        cache.push({hash: hash, value: result}) ; // добавляем элемент с правильной структурой
+        if (cache.length > 5) { 
+            cache.shift(); // если слишком много элементов в кеше, надо удалить самый старый (первый) 
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;  
+    }
+    return wrapper;
 }
+
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId = null;
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+
+    function wrapper(...args) {
+        wrapper.allCount++;
+        
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        
+        if (wrapper.allCount === 1) {
+            func.call(this, ...args);
+            wrapper.count++;
+        }
+
+        timeoutId = setTimeout(() => {
+            func.apply(this, [...args]);
+            clearTimeout(timeoutId);
+            timeoutId = null;
+            wrapper.count++;
+        }, delay);
+    }
+
+    return wrapper;
 }
